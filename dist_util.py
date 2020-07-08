c8006f0e6c0eb211de45a9287d66316ac056b0e3
at1047 <62730657+at1047@users.noreply.github.com>
2020-07-08 10:05:24 +0800

    optimised

--- dist_util.py
+++ dist_util.py
@@ -1,89 +1,63 @@
 import json
 
-
 class DistUtil():
 	def __init__(self):
 		pass
 
-	def key_change_helper(self, obj, keysToChange):
-		if isinstance(obj, list):
-			for dictEntry in obj:
-				self.key_change_helper(dictEntry, keysToChange)
-
-		elif isinstance(obj, dict):
-			for key in list(obj.keys()):
-				for keyPair in keysToChange.items():
-					if key == keyPair[0]:
-						obj[keyPair[1]] = obj.pop(key)
-					else:
-						if isinstance(obj.get(key), (dict, list)):
-							self.key_change_helper(obj[key], keysToChange)
-		return obj
-
 	def key_change(self, obj, keysToChange):
-		"""Changes specified key
-		
-		Args:
-		    obj ([list, dict]): List or dict to be edited
-		    keysToChange (dict): key name pair to be changed
-		
-		Returns:
-		    TYPE: [list, dict]
-		"""
-		return self.key_change_helper(obj, keysToChange)
+		jsoned = json.dumps(obj)
+		for keyPair in keysToChange.items():
+			jsoned = jsoned.replace(keyPair[0], keyPair[1])
+		return json.loads(jsoned)
 
-	def filter_name_helper(self, obj, whiteList):
+	def filter_name_inner(self, obj, whiteList):
 		if isinstance(obj, list): # obj must be list
 			for listEntry in obj:
-				self.filter_name_helper(listEntry, whiteList)
+				self.filter_name_inner(listEntry, whiteList)
 
 		elif isinstance(obj, dict): # obj must be dict
 			for key in [key for key in list(obj.keys()) if key not in whiteList]:
 				del obj[key]
 			for key in list(obj.keys()):
-				self.filter_name_helper(obj.get(key), whiteList)
-				
+				if whiteList[key] == 0:
+					self.filter_name_inner(obj.get(key), whiteList)
 		return obj
 
 	def filter_name(self, obj, whiteList):
-		"""Filters based on white list
-		
-		Args:
-		    obj ([list, dict]): List or dict to be edited
-		    whiteList (list): List of keys to be white listed
-		
-		Returns:
-		    TYPE: [list, dict]
-		"""
-		return self.filter_name_helper(obj, whiteList)
+		return self.filter_name_inner(obj, whiteList)
 
-# class ListMerger():
-# 	def __init__(self, listA, listB, fieldA="id", fieldB=None):
-# 		self.listA = listA
-# 		self.listB = listB
-# 		self.fieldA = fieldA
-# 		if fieldB == None:
-# 			self.fieldB = fieldA
-# 		else:
-# 			self.fieldB = fieldB
+	def find_value_diffkey_inner(self, obj, oldKey, value, newKey, final):
+		if isinstance(obj, list): # obj must be list
+			for listEntry in obj:
+				final = self.find_value_diffkey_inner(listEntry, oldKey, value, newKey, final)
+		elif isinstance(obj, dict):
+			if oldKey in obj and value == obj.get(oldKey):
+				final = obj.get(newKey)
+			else:
+				for key in list(obj.keys()):
+					final = self.find_value_diffkey_inner(obj.get(key), oldKey, value, newKey, final)
+		return final
 
-# 	def mergeId(self):
-# 		newList = []
-# 		for a in self.listA:
+	def find_value_samekey_inner(self, obj, oldKey, final):
+		if isinstance(obj, list): # obj must be list
+			for listEntry in obj:
+				final.append(self.find_value_samekey_inner(listEntry, oldKey, final))
+		elif isinstance(obj, dict):
+			if oldKey in obj:
+				final.append(obj.get(oldKey))
+			else:
+				for key in list(obj.keys()):
+					if isinstance(obj.get(key), (dict, list)):
+						final.append(self.find_value_samekey_inner(obj.get(key), oldKey, final))
+		return final
 
-# 			b = next((item for item in self.listB if item[self.fieldB] == a[self.fieldA]), None)
-
-# 			newData = {self.fieldA: a.pop(self.fieldA)}
-# 			for key in a:
-# 				newData[key] = a[key]
-
-# 			for key in b:
-# 				if key != self.fieldB:
-# 					newData[key] = b[key]
-# 			newList.append(newData)
-# 		return newList
-
-
+	def find_value(self, obj, oldKey, value=None, newKey=None):
+		final = None
+		if value == None and newKey == None:
+			final = []
+			return self.find_value_samekey_inner(obj, oldKey, final)
+		else:
+			return self.find_value_diffkey_inner(obj, oldKey, value, newKey, final)
 
 # Use main() for testing cases
 
@@ -91,43 +65,48 @@ def main():
 
 	# whiteList = ["id", "otherField", "newName", "otherName"]
 
-	# listA = [{
-	#             "id": 1,
-	#             "name": "first",
-	#             "otherField": [{
-	#             	"newName": "name3",
-	#             	"name": "second"
-	#             },
-	#             {
-	#             	"otherName": "otherName1"
-	#             }]
+	toChange = {
+	'name': 'Name',
+	'otherName': 'OTHERNAME'
+	}
+
+	listA = [{
+	            "id": 1,
+	            "name": "name1",
+	            "otherField": [{
+	            	"newName": "name3",
+	            	"name": "first"
+	            },
+	            {
+	            	"otherName": "otherName1"
+	            }]
 	            
-	#         },
-	#         {
-	#             "id": 2,
-	#             "name": "second",
-	#             "otherField": [{
-	#             	"newName": "name4",
-	#             	"name": "second"
-	#             }]
-	#         },
-	#         {
-	#             "id": 3,
-	#             "name": "second",
-	#             "otherField": [{
-	#             	"newName": "name4",
-	#             	"name": "second"
-	#             }]
-	#         }
-	#     ]
+	        },
+	        {
+	            "id": 2,
+	            "name": "name2",
+	            "otherField": [{
+	            	"newName": "name4",
+	            	"name": "second"
+	            }]
+	        },
+	        {
+	            "id": 3,
+	            "name": "name3",
+	            "otherField": [{
+	            	"newName": "name4",
+	            	"name": "third"
+	            }]
+	        }
+	    ]
 
 	# listA = {
 	#             "id": 1,
 	#             "name": "first",
 	#             "otherField": [{
-	#             	"newName": "name3"
+	#             	"newName": "name3",
+	#             	"temp": 1
 	#             }]
-	            
 	#         }
 
 
@@ -142,8 +121,8 @@ def main():
 	#         }
 	# ]
 	
-	distUtil = DistUtil()
-	print(json.dumps(distUtil.filter_name(listA, whiteList)))
+	du = DistUtil()
+	print(du.key_change(listA, toChange))
 
 if __name__ == "__main__":
     main()
